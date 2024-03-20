1bf9df286b590657330afe07518a42aeacb721e7
Your Name <you@example.com>
2024-03-20 10:17:46 +0200

    er

--- main.py
+++ main.py
@@ -15,16 +15,40 @@
 # - iespēja apskatīt vidējo izdevumu summu
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
+import json
 
-expenses = []
 
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
-pass
+expenses_file = open('expenses.json') # opening JSON file
+expenses = json.load(expenses_file) # returns JSON object as a dictionary
+expenses_file.close()
+
 
 while True:
     command = input("\nChoose command:")
     if command == "1":
+
+        name = input("the name of product: ")
+        sum = int(input("The sum is: "))
+        category = input("the name of category: ")
+        expense = {"name": name, "sum": sum, "category": category}
+        expenses.append(expense)
+        print(expenses)
+        with open("expenses.json", "w") as outfile:
+            json.dump(expenses, outfile)
+        pass
+    if command == "2":
+        def sort_sum(expense):
+            return int(expense["sum"])
+        expenses.sort(key = sort_sum, reverse = True)
+        print(expenses[:11])
+        pass
+    if command == "3":
+        def sort_sum(expense):
+            return int(expense["sum"])
+        expenses.sort(key = sort_sum, reverse = False)
+        print(expenses[:11])
         pass
     if command == "e":
         print("Exiting...")
